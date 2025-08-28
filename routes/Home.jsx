5a9806ae2399aaf42c4722cd029f798d0c186057
leeseoyoun <w2318@e-mirim.hs.kr>
2025-08-28 16:18:01 +0900

    wip: Home화면 진행상황 저장

--- routes/Home.jsx
+++ routes/Home.jsx
@@ -1,3 +1,34 @@
-function Home() {}
+import "../css/Home.scss";
+import Logo from "../src/assets/logo.png";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="logo-content">
+        <img src={Logo} className="logo-image"/>
+      </div>
+      <div className="calendar-content">
+        <div className="calendar-nav">
+          <p className="prev-month">◀</p>
+          <p className="current-month">MONTH</p>
+          <p className="next-month">▶</p>
+        </div>
+        <div className="week-content">
+          <p className="day">일</p>
+          <p className="day">월</p>
+          <p className="day">화</p>
+          <p className="day">수</p>
+          <p className="day">목</p>
+          <p className="day">금</p>
+          <p className="day">토</p>
+        </div>
+        <div className="calendar">
+
+        </div>
+      </div>
+      <div className="today-menu-list-content"></div>
+    </div>
+  );
+}
 
 export default Home;
